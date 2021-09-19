d1534c029e62403067e4cc25051346776c86fc60
Sergey Filatov <filatovs.vl@gmail.com>
2021-09-20 00:44:11 +0300

    На странице с друзьями, друзья выводятся по списку.

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -1,23 +1,37 @@
 import React from "react";
 
-const Friends = () => {
-  return (<table className="table">
-  <thead>
+const TableRow = (props) => {
+  return (
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">Имя и фамилия</th>
+      <th scope="row">{props.index + 1}</th>
+      <td>
+        {props.name} {props.lastname}
+      </td>
+    </tr>
+  );
+};
 
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-  </tbody>
-</table>)
+const Friends = (props) => {
+  let users = props.function();
+  let userCount = Object.keys(users).length;
+  let userRow = [];
+
+  for (let i = 0; i < userCount; i++) {
+    userRow.push(
+      <TableRow key={i} index={i} name={users[i].name} lastname={users[i].lastname} />
+    );
+  }
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Имя и фамилия</th>
+        </tr>
+      </thead>
+      <tbody>{userRow}</tbody>
+    </table>
+  );
 };
 
 export default Friends;
@@ -60,4 +74,3 @@ export default Friends;
 // );
 // }
 // });
-
